e735365178414a43382ed55b8395337a230e77d3
Chintan-elite <thecsquare.elite@gmail.com>
2023-03-15 09:21:14 +0530

    js

---
+++ JS_Learninig/array.js
@@ -0,0 +1,30 @@
+
+var a = [10, 20, 30, 30, 40, 50, 60]
+
+// console.log(a);
+// console.log(a.length);
+// a.push(50)
+// console.log(a);
+// a.shift()
+// console.log(a);
+// a.unshift(1000)
+// console.log(a);
+// a.pop()
+// console.log(a);
+
+
+const val = a.find(e => {
+    return e == 30;
+})
+
+const val1 = a.filter(e => {
+    return e == 30;
+})
+
+console.log(val);
+console.log(val1);
+
+
+
+
+
---
+++ JS_Learninig/function.js
@@ -0,0 +1,29 @@
+
+// function demo() {
+//     console.log("function calling...");
+// }
+
+// function add(a, b) {
+//     console.log(a + b);
+// }
+
+// function square(a) {
+//     return a * a;
+// }
+
+// function cube(a) {
+//     console.log(a * a * a);
+// }
+
+//demo()
+// add(10, 20)
+// add(50, 70)
+// var k = square(5)
+// console.log(k);
+// cube(5)
+
+const square = (a) => {
+    console.log(a * a);
+}
+
+square(10)
